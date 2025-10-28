80f6aa2e56870073a8f359f4078e8152a0c36a6e
abhaybhag <abhaybhagat913@gmail.com>
2025-10-28 16:58:38 +0530

    sixth commit

--- src/common/Cart.jsx
+++ src/common/Cart.jsx
@@ -16,8 +16,8 @@ export default function Cart() {
     return (
         <>
             <div className="container mx-auto mt-10">
-                <div className="flex shadow-md my-10">
-                    <div className="w-3/4 bg-white px-10 py-10">
+                <div className="flex shadow-md my-10 flex-wrap">
+                    <div className="w-3/4 bg-white px-10 py-10 lg:basis-[70%] basis-[100%]">
                         <div className="flex justify-between border-b pb-8">
                             <h1 className="font-semibold text-2xl">Shopping Cart</h1>
                             <h2 className="font-semibold text-2xl">3 Items</h2>
@@ -63,7 +63,7 @@ export default function Cart() {
                             Continue Shopping
                         </a>
                     </div>
-                    <div id="summary" className="w-1/4 px-8 py-10">
+                    <div id="summary" className="w-1/4 px-8 py-10 lg:basis-[30%] basis-[100%]">
                         <h1 className="font-semibold text-2xl border-b pb-8">Order Summary</h1>
                         <div className="flex justify-between mt-10 mb-5">
                             <span className="font-semibold text-sm uppercase">Items 3</span>
@@ -75,23 +75,8 @@ export default function Cart() {
                             <span className='uppercase'>{totalprice > 500  ? "standard shipping" + " " + `${shipping} \u20B9'` : ""}</span>
 
                         </div>
-                        <div className="py-10">
-                            <label
-                                htmlFor="promo"
-                                className="font-semibold inline-block mb-3 text-sm uppercase"
-                            >
-                                Promo Code
-                            </label>
-                            <input
-                                type="text"
-                                id="promo"
-                                placeholder="Enter your code"
-                                className="p-2 text-sm w-full"
-                            />
-                        </div>
-                        <button className="bg-red-500 hover:bg-red-600 px-5 py-2 text-sm text-white uppercase">
-                            Apply
-                        </button>
+                       
+                        
                         <div className="border-t mt-8">
                             <div className="flex font-semibold justify-between py-6 text-sm uppercase">
                                 <span>Total cost</span>
--- src/common/Productmyntra.jsx
+++ src/common/Productmyntra.jsx
@@ -353,7 +353,7 @@ let Productmyntra = () => {
                 <option value="8">Rating : High to Low</option>
               </select>
             </div>
-            <section className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+            <section className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4 mb-6">
 
               {loader ? <Loader /> : productdata.map((productsdata, i) => {
 
@@ -431,7 +431,7 @@ let Productmyntra = () => {
 
 
             </section>
-            <ResponsivePagination
+            <ResponsivePagination className='flex justify-center pb-3'
               current={currentPage}
               total={totalPages}
               onPageChange={SetcurrentPage}
